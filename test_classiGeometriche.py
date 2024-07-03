83fc9af0a9840b2a261c41e20e2742d87da5ee1b
Cosimo222 <166954814+Cosimo222@users.noreply.github.com>
2024-07-03 10:13:39 +0200

    Esercizio

---
+++ test_classiGeometriche.py
@@ -0,0 +1,7 @@
+import pytest, classiGeometriche
+
+def test_Poligono():
+    # 1. Creo un oggetto di tipo Poligono
+    # 2. Testo le sue funzionalità (i suoi metodi, i suoi attributi, i suoi metodi dunder, ed il suo tipo)
+    # NB: ricorda che puoi verificare con un assert il tipo di un oggetto con la funzione isinstance(nomeOggetto, tipo)
+    ...
