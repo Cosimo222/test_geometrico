29f2d9e04a768294f800acd832c4c6adb6eb8ee6
manuelper9 <150619214+manuelper9@users.noreply.github.com>
2024-07-03 10:48:32 +0200

    ciao
    ho inserito print()

--- test_classiGeometriche.py
+++ test_classiGeometriche.py
@@ -4,6 +4,7 @@ def test_Poligono():
     # 1. Creo un oggetto di tipo Poligono
     # 2. Testo le sue funzionalità (i suoi metodi, i suoi attributi, i suoi metodi dunder, ed il suo tipo)
     # NB: ricorda che puoi verificare con un assert il tipo di un oggetto con la funzione isinstance(nomeOggetto, tipo)
+    print()
     ...
 def test_Triangolo():
     pass    
